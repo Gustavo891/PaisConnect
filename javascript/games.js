82d2910786df370aa56f9f20aa9418d12316ccbf
Gustavo <gushd891@gmail.com>
2024-07-02 12:21:10 -0300

    final project

--- javascript/games.js
+++ javascript/games.js
@@ -7,7 +7,7 @@ function loadHighScore() {
     let highScore = localStorage.getItem('highScore');
     console.log(highScore)
     if (highScore === null) {
-        highScore = 5; // Se não houver pontuação máxima salva, inicie com 0
+        highScore = 0; // Se não houver pontuação máxima salva, inicie com 0
     }
 
     document.getElementById('highScore').innerText = highScore;
